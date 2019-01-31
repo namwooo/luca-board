63588e0f6d9444d91d13e330b7c288d9816e5fd2
Luca <luca@bluewhale.kr>
2019-01-31 14:29:13 +0900

    default value of is_published is True

--- app/posts/models.py
+++ app/posts/models.py
@@ -11,7 +11,7 @@ class Post(db.Model):
                          nullable=False)
     title = db.Column(db.String(120), nullable=False)
     body = db.Column(db.Text)
-    is_published = db.Column(db.Boolean, nullable=False, default=False)
+    is_published = db.Column(db.Boolean, nullable=False, default=True)
     like_count = db.Column(db.Integer, nullable=False, default=0)
     view_count = db.Column(db.Integer, nullable=False, default=0)
     created_at = db.Column(db.DateTime, nullable=False,
@@ -19,9 +19,26 @@ class Post(db.Model):
     updated_at = db.Column(db.DateTime, nullable=False,
                            server_default=func.now(),
                            server_onupdate=func.now())
+    image = db.relationship('PostImage', backref='image', lazy=True)
 
     def __str__(self):
-        return '{}'.format(self.title)
+        return f'{self.title}'
 
     def __repr__(self):
-        return '<Post {}>'.format(self.title)
+        return f'<Post {self.title}>'
+
+
+class PostImage(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'),
+                        nullable=False)
+    image_url = db.Column(db.String(100), nullable=False)
+    caption = db.Column(db.String(30), nullable=False, default='')
+    created_at = db.Column(db.DateTime, nullable=False,
+                           server_default=func.now())
+
+    def __str__(self):
+        return f'{self.image_url}'
+
+    def __repr__(self):
+        return f'<PostImage {self.image_url}>'
