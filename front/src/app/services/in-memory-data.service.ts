3341ec55dbc9d4bf0dce1ebd4e363b21a0f5f6bd
Luca <luca@bluewhale.kr>
2019-02-15 14:43:15 +0900

    basic css for boards-list

--- front/src/app/services/in-memory-data.service.ts
+++ front/src/app/services/in-memory-data.service.ts
@@ -6,16 +6,16 @@ import { Injectable } from '@angular/core';
 export class InMemoryDataService {
   createDb() {
     const boards = [
-      { id: 1, title: 'Company life' },
-      { id: 2, title: 'Recruit' },
-      { id: 3, title: 'Blue Whale' },
-      { id: 4, title: 'University' },
-      { id: 5, title: 'Wanted' },
-      { id: 6, title: 'News' },
-      { id: 7, title: 'Employee' },
-      { id: 8, title: 'Employer' },
-      { id: 9, title: 'Trend' },
-      { id: 10, title: 'Money' }
+      { id: 1, title: '서류합격스펙' },
+      { id: 2, title: '합격자소서' },
+      { id: 3, title: '인적성 필기후기' },
+      { id: 4, title: '면접후기' },
+      { id: 5, title: '최종합격스펙' },
+      { id: 6, title: '최종합격후기' },
+      { id: 7, title: '공채속보' },
+      { id: 8, title: '채용공고' },
+      { id: 9, title: '중견중소' },
+      { id: 10, title: '인턴공고' }
     ];
     return {boards};
   }
