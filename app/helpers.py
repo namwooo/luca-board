7438161b6edab069cf98e68817660d2aec8822a0
Luca <luca@bluewhale.kr>
2019-03-04 15:31:49 +0900

    add helper to change case convention

---
+++ app/helpers.py
@@ -0,0 +1,33 @@
+import re
+
+
+def camel_to_snake(name):
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+
+
+def snake_to_camel(name):
+    under_pat = compile(r'_([a-z])')
+    return under_pat.sub(lambda x: x.group(1).upper(), name)
+
+
+def change_dict_naming_convention(d, convert_function):
+    """
+    Convert a nested dictionary from one convention to another.
+    Args:
+        d (dict): dictionary (nested or not) to be converted.
+        convert_function (func): function that takes the string in one convention and returns it in the other one.
+    Returns:
+        Dictionary with the new keys.
+    """
+    new = {}
+    for k, v in d.items():
+        new_v = v
+        if isinstance(v, dict):
+            new_v = change_dict_naming_convention(v, convert_function)
+        elif isinstance(v, list):
+            new_v = list()
+            for x in v:
+                new_v.append(change_dict_naming_convention(x, convert_function))
+        new[convert_function(k)] = new_v
+    return new
