1c7d0c23ecaa741915ff810cff7d79634c33eec1
Luca <luca@bluewhale.kr>
2019-03-11 11:17:02 +0900

    position login form to center

--- front/src/app/member/member-routing.module.ts
+++ front/src/app/member/member-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { ContentOnlyLayoutComponent } from './components/content-only-layout/content-only-layout.component';
 
 const routes: Routes = [
-    { path: 'login', component: LoginComponent },
+    { 
+      path: '', component: ContentOnlyLayoutComponent,  
+      children: [    
+          { path: 'login', component: LoginComponent },
+      ]
+  },
 ];
 
 @NgModule({
